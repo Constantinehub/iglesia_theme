d419ced40b2739e0c4f1a35d11d299e028678801
Constantine Shibaev <killtobanana@gmail.com>
2018-02-27 23:03:55 +0200

    Created header box without search_box (html work)

--- header.php
+++ header.php
@@ -10,51 +10,23 @@
 </head>
 <body <?php body_class(); ?> >
 
-<?php /*if(ale_get_option('sitelogo')){
-    echo "<img src='".ale_get_option('sitelogo')."' />";
-}*/ ?>
-<?php ale_option('sitelogo'); ?>
+<div class="wrapper">
+    <header class="top_header">
+        <div class="container cf">
+            <div class="header_third search_section menu_button">
+                search
+            </div>
+            <div class="header_third logo_section">
+                <a href="<?php echo home_url("/"); ?>" class="logo_link"><img src="<?php echo ale_get_option('sitelogo'); ?>" alt="" /></a>
+            </div>
+            <div class="header_third social_section">
+                <?php if(ale_get_option('vm')) { ?><a href="<?php echo ale_get_option('vm'); ?>" class="social_link"><i class="fab fa-vimeo-v"></i></a><?php } ?>
+                <?php if(ale_get_option('insta')) { ?><a href="<?php echo ale_get_option('insta'); ?>" class="social_link"><i class="fab fa-instagram"></i></a><?php } ?>
+                <?php if(ale_get_option('twi')) { ?><a href="<?php echo ale_get_option('twi'); ?>" class="social_link"><i class="fab fa-twitter-square"></i></a><?php } ?>
+                <?php if(ale_get_option('fb')) { ?><a href="<?php echo ale_get_option('fb'); ?>" class="social_link"><i class="fab fa-facebook-f"></i></a><?php } ?>
+            </div>
 
-<section class="slider-example">
-	<div class="newhomeslider wrapper">
-		<ul class="slides">
-			<?php $slider = ale_sliders_get_slider('test-slider');  ?>
-			<?php if($slider):?>
-				<?php foreach ($slider['slides'] as $slide) : ?>
-					<li>
-						<figure>
-							<img src="<?php echo $slide['image']; ?>" alt="<?php echo $slide['title']; ?>" />
-							<figcaption>
-								<div class="sliderdata">
-									<?php if($slide['title']){ ?>
-										<div class="titleslide headerfont">
-											<?php if($slide['url']){
-												echo "<a href='".$slide['url']."'>";
-											} ?>
-
-											<?php echo $slide['title']; ?>
-
-											<?php if($slide['url']){
-												echo "</a>";
-											} ?>
-										</div>
-									<?php } ?>
-									<?php if($slide['description']){ ?>
-										<div class="descriptionslide">
-											<?php echo $slide['description']; ?>
-										</div>
-									<?php } ?>
-									<?php if($slide['html']){ ?>
-										<div class="descriptionslide">
-											<?php echo $slide['html']; ?>
-										</div>
-									<?php } ?>
-								</div>
-							</figcaption>
-						</figure>
-					</li>
-				<?php endforeach; ?>
-			<?php endif;?>
-		</ul>
-	</div>
-</section>
+        </div>
+    </header>
+    <section class="home_slider"></section>
+</div>
