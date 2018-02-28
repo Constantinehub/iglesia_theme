f2320901a6c931caca94f9b69096ba6498541f8a
Constantine Shibaev <killtobanana@gmail.com>
2018-02-28 21:04:48 +0200

    Created header box (added top menu)

--- header.php
+++ header.php
@@ -13,8 +13,14 @@
 <div class="wrapper">
     <header class="top_header">
         <div class="container cf">
-            <div class="header_third search_section menu_button">
-                search
+            <div class="header_third search_section menu_button cf">
+                <div class="menu_icon">
+                    <i class="fas fa-bars"></i>
+                    <i class="fas fa-times" style="display: none;"></i>
+                </div>
+                <div class="search_box">
+                    <?php echo get_search_form(); ?>
+                </div>
             </div>
             <div class="header_third logo_section">
                 <a href="<?php echo home_url("/"); ?>" class="logo_link"><img src="<?php echo ale_get_option('sitelogo'); ?>" alt="" /></a>
@@ -25,8 +31,25 @@
                 <?php if(ale_get_option('twi')) { ?><a href="<?php echo ale_get_option('twi'); ?>" class="social_link"><i class="fab fa-twitter-square"></i></a><?php } ?>
                 <?php if(ale_get_option('fb')) { ?><a href="<?php echo ale_get_option('fb'); ?>" class="social_link"><i class="fab fa-facebook-f"></i></a><?php } ?>
             </div>
-
         </div>
+        <nav class="top_navigation">
+            <div class="container">
+                <?php
+                if ( has_nav_menu( 'header_menu' ) ) {
+                    wp_nav_menu(array(
+                        'theme_location'=> 'header_menu',
+                        'menu'			=> 'Header Menu',
+                        'menu_class'	=> 'ale_headermenu cf',
+                        'walker'		=> new Aletheme_Nav_Walker(),
+                        'container'		=> '',
+                    ));
+                }
+                ?>
+                <div class="donate_button">
+                    <a href="<?php //echo ale_get_option('donate_link'); ?>"><?php _e('Donate', 'iglesia'); ?></a>
+                </div>
+            </div>
+        </nav>
     </header>
     <section class="home_slider"></section>
 </div>
